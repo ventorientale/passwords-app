922e36be20628c0d3218fafeaca1764999fefce4
greenplugin <wformps@gmail.com>
2019-10-14 04:28:22 +0700

    fixed production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,15 @@
-import {environment as environmentDefault} from './environment';
+import {ENCRYPTION_AES_CTR} from '../app/constants/encryption-types';
 
 export const environment = {
-  ...environmentDefault,
   production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCVB7z_Qjw2axFVCFayJkZiye4v_IfE-JM',
+    authDomain: 'passwords-app.firebaseapp.com',
+    databaseURL: 'https://passwords-app.firebaseio.com',
+    projectId: 'passwords-app',
+    storageBucket: '',
+    messagingSenderId: '349391034347',
+    appId: '1:349391034347:web:897b033f37517917902bae'
+  },
+  defaultEncryptionDriver: ENCRYPTION_AES_CTR,
 };
